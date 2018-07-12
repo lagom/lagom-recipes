1e24f73140008312d296f3482c667af825834d1e
Renato  Cavalcanti <renato@cavalcanti.be>
2018-07-12 22:37:54 +0200

    appending different play routers to lagom server

--- file-upload/file-upload-scala-sbt/fileupload-impl/src/main/scala/com/example/fileupload/impl/FileuploadLoader.scala
+++ file-upload/file-upload-scala-sbt/fileupload-impl/src/main/scala/com/example/fileupload/impl/FileuploadLoader.scala
@@ -1,13 +1,18 @@
 package com.example.fileupload.impl
 
+import akka.cluster.Cluster
+import akka.management.cluster.ClusterHttpManagementRoutes
 import com.example.fileupload.api.FileUploadService
 import com.example.play.controllers.FileUploadController
 import com.lightbend.lagom.scaladsl.api.ServiceLocator
 import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
+import com.lightbend.lagom.scaladsl.cluster.ClusterComponents
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
+import com.lightbend.lagom.scaladsl.playjson.{EmptyJsonSerializerRegistry, JsonSerializerRegistry}
 import com.lightbend.lagom.scaladsl.server._
 import com.softwaremill.macwire._
 import play.api.libs.ws.ahc.AhcWSComponents
+import play.api.routing.AkkaHttpPlayRouter
 import router.Routes
 
 class FileUploadLoader extends LagomApplicationLoader {
@@ -15,6 +20,7 @@ class FileUploadLoader extends LagomApplicationLoader {
   override def load(context: LagomApplicationContext): LagomApplication =
     new FileUploadApplication(context) {
       override def serviceLocator: ServiceLocator = NoServiceLocator
+
     }
 
   override def loadDevMode(context: LagomApplicationContext): LagomApplication =
@@ -25,32 +31,26 @@ class FileUploadLoader extends LagomApplicationLoader {
 
 abstract class FileUploadApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
+    with ClusterComponents
     with AhcWSComponents {
 
-  // Bind the service that this server provides
-  override lazy val lagomServer = serverFor[FileUploadService](wire[FileUploadServiceImpl])
+  override def jsonSerializerRegistry: JsonSerializerRegistry = EmptyJsonSerializerRegistry
 
+  lazy implicit val ac = actorSystem
+  lazy val clusterHttpRouter = AkkaHttpPlayRouter("/cluster", ClusterHttpManagementRoutes(Cluster(ac)))
 
-  // We override the default `router` Lagom builds from the user-provided
-  // `lagomServer`. The new Router is actually an instance of the result
-  // of compiling the `routes` file in `src/main/resources`.
-  //
-  // The router is built using an httpErrorHandler and a list of Routes.
-  // the list of Routes depends on the contents of the `src/main/resources/routes`
-  // file. In our case, the `src/main/resources/routes` declares:
-  //
-  //  POST    /api/files      com.example.play.controllers.FileUploadController.uploadFile()
-  //  ->      /               com.lightbend.lagom.scaladsl.server.LagomServiceRouter
-  //
-  // which translates into `new Routes(...)` needing an instance of a
-  // FileUploadController and an instance of the default lagom router.
-  //
-  // This is a Play provided feature. To know more, see:
-  //     https://www.playframework.com/documentation/2.6.x/ScalaCompileTimeDependencyInjection#Providing-a-router
-  override lazy val router = new Routes(
+  lazy val fileUploadRouter = new Routes(
     httpErrorHandler,
-    new FileUploadController(controllerComponents),
-    lagomServer.router
+    new FileUploadController(controllerComponents)
   )
+
+  // Bind the service that this server provides
+  override lazy val lagomServer: LagomServer =
+    serverFor[FileUploadService](wire[FileUploadServiceImpl])
+      .plusRouter(clusterHttpRouter)
+      .plusRouter(fileUploadRouter.withPrefix("/io"))
+
+
+
 
 }
