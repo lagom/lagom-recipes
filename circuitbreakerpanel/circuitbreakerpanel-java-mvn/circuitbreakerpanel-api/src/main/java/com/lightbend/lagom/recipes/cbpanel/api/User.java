40a402316ed515ce9ddd1d9c2f762d673b413ed1
neelaksh <-->
2018-08-12 15:18:21 +0530

    Made clas fields private-final and added getters

--- circuitbreakerpanel/circuitbreakerpanel-java-mvn/circuitbreakerpanel-api/src/main/java/com/lightbend/lagom/recipes/cbpanel/api/User.java
+++ circuitbreakerpanel/circuitbreakerpanel-java-mvn/circuitbreakerpanel-api/src/main/java/com/lightbend/lagom/recipes/cbpanel/api/User.java
@@ -2,9 +2,21 @@ package com.lightbend.lagom.recipes.cbpanel.api;
 
 public class User {
     
-    String name;
-    Integer id;
-    String location;
+    private final String name;
+    private final Integer id;
+    private final String location;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public String getLocation() {
+        return location;
+    }
     
     public User(String name, Integer id, String location) {
         this.name = name;
